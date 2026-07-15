2c4d2b5e696abd6c57d46a340fa40a7c5e1b2685
Dhanush K <kdhanush681@gmail.com>
2026-07-15 16:10:37 +0530

    Update handling_na.py

--- data_preparation/handling_na.py
+++ data_preparation/handling_na.py
@@ -1,12 +1,12 @@
 import pandas as pd
-# df=pd.read_csv("clean_olist_products_dataset.csv")
-# for column in df.columns:
-#     # Check if the column is of object type (string)
-#     if df[column].dtype == 'object':
-#         df[column].fillna('unknown', inplace=True)
-#     elif df[column].dtype in ['int64', 'float64']:
-#         df[column].fillna(df[column].median(), inplace=True)
-# df.to_csv("clean_olist_products_dataset.csv", index=False, encoding='utf-8')
+df=pd.read_csv("clean_olist_products_dataset.csv")
+for column in df.columns:
+    # Check if the column is of object type (string)
+    if df[column].dtype == 'object':
+        df[column].fillna('unknown', inplace=True)
+    elif df[column].dtype in ['int64', 'float64']:
+        df[column].fillna(df[column].median(), inplace=True)
+df.to_csv("clean_olist_products_dataset.csv", index=False, encoding='utf-8')
 df=pd.read_csv("clean_olist_order_reviews_dataset.csv", usecols=["review_id", "order_id", "review_score", "review_creation_date", "review_answer_timestamp"])
 for column in df.columns:
     # Check if the column is of object type (string)
